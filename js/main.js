14b45dd40049e9751534e822ffa997f496763a43
beaferreira <triszferreira@gmail.com>
2021-09-14 12:40:07 -0300

    Introdução ao Java Script

---
+++ js/main.js
@@ -0,0 +1,87 @@
+function clicou(){
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigado por clicar</b>";
+    console.log(document.getElementById("agradecimento"));
+    //alert("Obrigado por clicar");
+}
+
+function redirecionar(){
+    window.open("https://digitalinnovation.one/");
+}
+
+
+/*
+function soma (n1, n2){
+    return n1 + n2;
+}
+*/
+/*
+function validaIdade(idade){
+    var validar;
+    if (idade >= 18){
+        validar = true;
+    }else{
+        validar = false;
+    }
+    return validar; 
+}
+
+var idade = prompt("Qual sua idade?");
+console.log(validaIdade(idade));
+*/
+/*function setReplace(frase, nome, novo_nome){
+    return frase.replace(nome, novo_nome);
+}
+*/
+//alert(soma (5, 10));
+//alert(setReplace("Vai Japão", "Japão", "Brasil"));
+
+/*var d= new Date();
+alert(d);
+*/
+/*var count;
+for(count=0; count<=5; count++){
+    alert(count);
+};
+*/
+/*var count = 0;
+while (count <= 5){
+    console.log(count);
+    alert(count);
+    count++;
+}
+*/
+/*var idade = prompt("Qual sua idade? ");
+
+if (idade >= 18){
+    alert("maior de idade");
+}else{
+    alert("menor de idade");
+}
+*/
+/*var frutas = [{nome: "maçã", cor: "vermelha"} , {nome: "uva", cor: "roxa"}]; 
+console.log(frutas);
+alert(frutas[1].nome);
+*/
+/*var fruta = {nome: "maçã", cor: "vermelha"};
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
+
+//var lista = ["maçã", "pera", "laranja"]
+//lista.push("uva")
+//lista.pop();
+
+//console.log(lista.length);
+//console.log(lista.reverse());
+//console.log(lista[0]);
+//console.log(lista.toString());
+//console.log(lista.join(" - "))
+//var nome = "Beatriz Ferreira";
+//var idade = 18;
+//var idade2 = 10;
+//var frase = "Japão é o melhor time do mundo";
+//alert(nome + " tem " + idade + " anos");
+//alert(idade + idade2);
+//console.log(nome);
+//console.log(idade + idade2);    
+//console.log(frase.replace("Japão" , "Brasil"));
